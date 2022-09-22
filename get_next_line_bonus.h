38c43a5d016a973553b85f6b4a51c0ce74ad1282
rokabal <26251909+rokabal@users.noreply.github.com>
2022-09-22 19:45:16 +0400

    added buffer_size if not def

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -13,6 +13,10 @@
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4096
+# endif
+
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
